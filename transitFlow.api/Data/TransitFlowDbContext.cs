73efefe68a01856c0663706adcdd9ed6c36fca3c
codye1 <123339584+codye1@users.noreply.github.com>
2026-06-22 09:48:36 +0300

    #TRN-3 connected db

---
+++ transitFlow.api/Data/TransitFlowDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace transitFlow.api.Data
+{
+    public class TransitFlowDbContext : DbContext
+    {
+        public TransitFlowDbContext(DbContextOptions<TransitFlowDbContext> options) : base(options)
+        {
+        }
+
+    }
+}
