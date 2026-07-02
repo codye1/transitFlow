0dc8a8f1c87672d2969fbb46bfd48610a1a377b5
codye1 <123339584+codye1@users.noreply.github.com>
2026-07-02 09:01:28 +0300

    TRN-31 move svg to images

--- transitionFlow.mvc/obj/Debug/net8.0/transitFlow.mvc.AssemblyInfo.cs
+++ transitionFlow.mvc/obj/Debug/net8.0/transitFlow.mvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TransitFlow.mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4b9c06007c526a9da01c61e0e5b6df98b9350f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c822afb07f11a70eff302fcdecb6131936fbaf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TransitFlow.mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TransitFlow.mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
